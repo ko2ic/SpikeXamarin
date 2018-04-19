05b3e0b8023c6aff18f636b398f622cb49ac6909
kouji ishii <k-ishii@eole.co.jp>
2018-04-19 11:03:21 +0900

    for preview

--- SpikeXamarin/ViewModels/FirstViewModel.cs
+++ SpikeXamarin/ViewModels/FirstViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using SpikeXamarin.Domains.Entities;
 using SpikeXamarin.Domains;
 using SpikeXamarin.Infrastructures.Repositories.Http;
+using System.Collections.Generic;
 
 namespace SpikeXamarin.ViewModels
 {
@@ -27,13 +28,6 @@ namespace SpikeXamarin.ViewModels
 
         public void OnAppearing()
         {
-            //var list = Enumerable.Range(0, 100).Select(n => new RepoEntity
-            //{
-            //    Id = n,
-            //    Name = "Name" + n,
-            //    FullName = "FullName" + n,
-            //    Stars = n
-            //});
 
 
         }
@@ -44,6 +38,36 @@ namespace SpikeXamarin.ViewModels
             foreach (var item in result.items)
             {
                 Items.Add(item);
+            }
+        }
+    }
+
+    public static class ViewModelLocator
+    {
+        static FirstViewModel firstViewModel;
+
+        public static FirstViewModel FirstViewModel
+        {
+            get
+            {
+                if (firstViewModel == null)
+                {
+                    var list = Enumerable.Range(0, 100).Select(n => new RepoEntity
+                    {
+                        Id = n,
+                        Name = "Name" + n,
+                        FullName = "FullName" + n,
+                        Stars = n
+                    });
+                    firstViewModel = new FirstViewModel();
+                    foreach (var item in list)
+                    {
+                        firstViewModel.Items.Add(item);
+                    }
+                }
+
+                return firstViewModel;
+
             }
         }
     }
